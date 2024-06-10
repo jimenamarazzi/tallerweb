3d5356c61d76745d785052176931773295a9a984
jimenamarazzi <130176223+jimenamarazzi@users.noreply.github.com>
2024-06-10 02:35:44 -0300

    Add files via upload

---
+++ CinefilosWeb/script.js
@@ -0,0 +1,12 @@
+const ontenedor1 = document.getElementById('contenedor1');
+
+        function scrollIzquierda() {
+            contenedor1.scrollLeft -= 100; // Mueve el scroll hacia la izquierda
+        }
+
+        function scrollDerecha() {
+            contenedor1.scrollLeft += 100; // Mueve el scroll hacia la derecha
+        }
+
+
+        
